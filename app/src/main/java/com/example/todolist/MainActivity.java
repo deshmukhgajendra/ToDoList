cba7a8cade379e648fbd06e5be83c1374dd014ef
gajendra <gajendradeshmukh838@gmail.com>
2024-09-05 19:14:34 +0530

    fifth commit

--- app/src/main/java/com/example/todolist/MainActivity.java
+++ app/src/main/java/com/example/todolist/MainActivity.java
@@ -1,19 +1,32 @@
 package com.example.todolist;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.todolist.MyDay.MyDay;
+import com.example.todolist.Planned.Planned;
+import com.example.todolist.Task.Tasks;
+import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.bottomappbar.BottomAppBar;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+
+
 public class MainActivity extends AppCompatActivity {
+    MaterialToolbar toolbar;
+    BottomAppBar bottom_app_bar;
+    Button mydayButton,takskButton;
+    Button plannedButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +37,36 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        toolbar=findViewById(R.id.toolbar);
+        bottom_app_bar=findViewById(R.id.bottom_app_bar);
+        mydayButton=findViewById(R.id.mydayButton);
+        plannedButton=findViewById(R.id.plannedButton);
+        takskButton=findViewById(R.id.tasksButton);
+
+
+        mydayButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent= new Intent(MainActivity.this, MyDay.class);
+                startActivity(intent);
+            }
+        });
+        plannedButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent= new Intent(MainActivity.this, Planned.class);
+                startActivity(intent);
+            }
+        });
+
+        takskButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent= new Intent(MainActivity.this, Tasks.class);
+                startActivity(intent);
+            }
         });
 
 
--- app/src/main/java/com/example/todolist/MyDay/MyDay.java
+++ app/src/main/java/com/example/todolist/MyDay/MyDay.java
@@ -1,6 +1,11 @@
 package com.example.todolist.MyDay;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,8 +14,15 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.todolist.R;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.radiobutton.MaterialRadioButton;
 
 public class MyDay extends AppCompatActivity {
+
+    FloatingActionButton fab;
+    public String task;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +33,37 @@ public class MyDay extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        fab=findViewById(R.id.fab);
+
+//        LayoutInflater inflater=getLayoutInflater();
+//        View view= inflater.inflate(R.layout.bottonsheetlayout,null,false);
+//        RadioButton taskButton = view .findViewById(R.id.tasksButton);
+//        Button addButton= view .findViewById(R.id.addButton);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LayoutInflater inflater = getLayoutInflater();
+                View bottomSheetView = inflater.inflate(R.layout.bottonsheetlayout, null);
+
+                EditText taskEditText=view.findViewById(R.id.taskEditText);
+                MaterialRadioButton taskButton=view.findViewById(R.id.tasksButton);
+                Button addButton=view.findViewById(R.id.addButton);
+
+                // Create and show the BottomSheetDialog
+                BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(MyDay.this);
+                bottomSheetDialog.setContentView(bottomSheetView);
+                bottomSheetDialog.show();
+
+                addButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        task=taskEditText.getText().toString();
+                    }
+                });
+            }
         });
     }
 }
